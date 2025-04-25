7094eff023854b0b0176395c882c940158ba8942
sirann17 <siranchen94@gmail.com>
2025-04-24 21:04:30 -0700

    hi

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,34 +1,31 @@
 // index.js - purpose and description here
-// Author: Siran Chen
-// Date: April 22, 2025
+/**
+ * Author: Siran Chen
+ * Created: April 24, 2025
+ * License: Public Domain
+ */
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
-// define variables
 make = "Ford";
 model = "Taurus";
 color = "Rusty";
-year = 1995;
-//calculate
-age = 2019 - year;
+carYear = 1995;
+currentYear = 2025;
+
+ownIt = true;
+age = currentYear - carYear;
+
+function myFunction(param1, param2){
+// some code here
+//return results;
+}
+
+function main(){
+// the code that makes everything happen
+
 //output
-document.writeln("Make: "+ make + "<br>");
+document.writeln("Make: " + make + "<br>");
 document.writeln("Model: " + model + "<br>");
 document.writeln("Color: " + color + "<br>");
-document.writeln("Year: " + year + "<br>");
-document.writeln("Age: " + age + " years<br>");
+document.writeln("Year: " + carYear + "<br>");
+document.writeln("Age: " + age + "<br>");
+}
