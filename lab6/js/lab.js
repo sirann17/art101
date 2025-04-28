065480f679527f34605ef33c28fe5cbdc7fb06d6
sirann17 <siranchen94@gmail.com>
2025-04-28 15:33:40 -0700

    hi

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Siran Chen
+// Created: April 28, 2025
+
 
 // Constants
 
@@ -15,7 +15,28 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+
+  let myTransport = [" car", " bus", " walk"];
+
+  let myMainRide = {
+    make: "Ford",
+    model: "Taurus",
+    color: "rusty",
+    year: 1995,
+    age: function() {
+        return 2025 - 1995;
+    }
+};
+
+//output
+document.writeln("Kinds of transportation I use: ", myTransport, "<br>");
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide.make, null, '\t'),
+   "</pre>");
+
 }
 
 // let's get this party started
 main();
+
+
