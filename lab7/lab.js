22fba21756eca5774b8c037101ea67d57a1b361d
sirann17 <siranchen94@gmail.com>
2025-05-02 17:49:44 -0700

    hi

--- lab7/lab.js
+++ lab7/lab.js
@@ -1,4 +1,20 @@
 // Author: Siran Chen
 //Created: April 29, 2025
 
-let userName = window.prompt();
+function sortUserName(){
+    let userName = window.prompt("Please tell me your name");
+    console.log("userName", userName);
+
+    let nameArray =userName.split('');
+    console.log("nameArray", nameArray);
+
+    let nameArraySort = nameArray.sort();
+    console.log("nameArraySort", nameArraySort);
+
+    let nameSorted = nameArraySort.join('');
+    console.log("nameSorted", nameSorted);
+
+    return nameSorted;
+}
+
+document.writeln("Hey, here is your name: ", sortUserName(), "</br>");
